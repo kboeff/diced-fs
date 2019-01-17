53f3d410f999fe65c52c39ffee00b99d377b0883
Kamen Boev <kamen.boev@gmail.com>
2019-01-17 10:28:45 +0000

    ESLint and pre-commit added

--- index.js
+++ index.js
@@ -4,7 +4,16 @@
 
 const logSomething = options => ({
   ...options,
-  anotherOption: 'Hello!'
+  anotherOption: 'Hello!',
 });
+
+class Bong {
+  constructor(name, level) {
+    this.name = name;
+    this.level = level;
+  }
+}
+
+const Bongo = new Bong('Bongito', 1);
 const options = logSomething({ one: '1', two: '2' });
-console.log(options);
+console.log(options, Bongo);
