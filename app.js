7e7bc399fbf2f79b48d280166a2d39d9de5883eb
Kamen Boev <kamen.boev@gmail.com>
2019-01-18 13:54:14 +0000

    dotenv added

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 /**
  * app.js
  */
+const dotenv = require('dotenv');
+
+// use dotenv as early as possible
+dotenv.config({
+    silent: true,
+});
+ 
 const path = require('path');
 const express = require('express');
 const routes = require('./routes/index');
@@ -12,8 +19,18 @@ const app = express();
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'pug');
 
+// serve static files from 'public'
+app.use(express.static(path.join(__dirname, './public')));
+
 // use routes
 app.use('/', routes);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
 // error handlers
 app.use((err, req, res, next) => {
