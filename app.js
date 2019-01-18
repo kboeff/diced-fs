56aae10fbf7061382aae3c5ccda5aed3f066c302
Kamen Boev <kamen.boev@gmail.com>
2019-01-18 14:43:47 +0000

    some minor installs

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ const dotenv = require('dotenv');
 
 // use dotenv as early as possible
 dotenv.config({
-    silent: true,
+  silent: true,
 });
- 
+const logger = require('morgan');
 const path = require('path');
 const express = require('express');
 const routes = require('./routes/index');
 
 // Express app setup
 const app = express();
+
+// logger
+app.use(logger('combined'));
 
 // view engine
 app.set('views', path.join(__dirname, './views'));
@@ -27,9 +30,9 @@ app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
